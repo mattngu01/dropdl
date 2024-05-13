22bfecde83390deaa2a25fb0bd6652a908e30cd9
Matthew Nguyen <mnguy1231@gmail.com>
2024-05-13 10:49:09 -0700

    Ruff formatting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dropbox.exceptions import AuthError, ApiError
 import sys
 from pathlib import Path
 import os
- 
+
 # Add OAuth2 access token here.
 # You can generate one for yourself in the App Console.
 # See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
@@ -16,20 +16,24 @@ REMOTE_PATH = "/Apps/remotely-save/Personal Vault"
 # relative location...?
 DEST_PATH = "/home/fareed/code/quartz/content"
 
+
 def get_remote_files(path: str) -> list[str]:
     with dropbox.Dropbox(TOKEN) as dbx:
         result = dbx.files_list_folder(path, recursive=True)
         return [file.path_display for file in result.entries]
 
+
 def get_dest_files(path: str) -> list[str]:
     return [str(x) for x in Path(path).rglob("*")]
 
-def get_diff(remote_path: str, dest_path: str, remote_files: list[str], dest_files: list[str]) -> list[str]:
+
+def get_diff(
+    remote_path: str, dest_path: str, remote_files: list[str], dest_files: list[str]
+) -> list[str]:
     remote_files = set(x.removeprefix(remote_path) for x in remote_files)
     dest_files = set(x.removeprefix(dest_path) for x in dest_files)
 
     return list(remote_files.difference(dest_files))
-
 
 
 print("Dropbox")
