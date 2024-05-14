94cae7244e0fa29ad8550390a56f872a61cf4dff
Matthew Nguyen <mnguy1231@gmail.com>
2024-05-13 17:58:38 -0700

    feat: Set modified / access time for downloaded files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import dropbox
 from dropbox.exceptions import AuthError, ApiError
+from dropbox.files import FileMetadata
 import sys
 from pathlib import Path
 import os
@@ -19,14 +20,10 @@ REMOTE_PATH = "/Apps/remotely-save/Personal Vault"
 DEST_PATH = "/home/matt/garden/content"
 
 
-def list_remote_files_recursive(path: str) -> list[str]:
+def list_remote_files_recursive(path: str) -> list[FileMetadata]:
     with dropbox.Dropbox(TOKEN) as dbx:
         result = dbx.files_list_folder(path, recursive=True)
-        return [
-            file.path_display
-            for file in result.entries
-            if isinstance(file, dropbox.files.FileMetadata)
-        ]
+        return [file for file in result.entries if isinstance(file, FileMetadata)]
 
 
 def list_remote_dirs_recursive(path: str) -> list[str]:
@@ -65,12 +62,19 @@ def download_folder(remote_path: str):
         os.mkdir(dl_folder + d)
 
     with dropbox.Dropbox(TOKEN) as dbx:
-        for f in files:
-            stripped = f.removeprefix(REMOTE_PATH)
+        for file in files:
+            stripped = file.path_display.removeprefix(REMOTE_PATH)
             dl_path = dl_folder + stripped
 
-            dbx.files_download_to_file(download_path=dl_path, path=f)
-            print(f"Downloaded {f} to {dl_path}")
+            dbx.files_download_to_file(download_path=dl_path, path=file.path_display)
+            os.utime(
+                dl_path,
+                times=(
+                    file.client_modified.timestamp(),
+                    file.client_modified.timestamp(),
+                ),
+            )
+            print(f"Downloaded {file.path_display} to {dl_path}")
 
 
 print("Dropbox")
@@ -80,9 +84,6 @@ print(remote_files)
 print("DIR")
 dest_files = list_dest_dir(DEST_PATH)
 print(dest_files)
-
-print("difference")
-print(get_diff(REMOTE_PATH, DEST_PATH, remote_files, dest_files))
 
 print("\n\n")
 download_folder(REMOTE_PATH)
